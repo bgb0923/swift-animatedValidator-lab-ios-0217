d6cf9685ec451d029a099f9dbdf006ed7426909f
bgb0923 <bgb0923@gmail.com>
2017-02-28 13:07:01 -0500

    Done

--- animated-validator-swift/ValidatorViewController.swift
+++ animated-validator-swift/ValidatorViewController.swift
@@ -43,6 +43,7 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
                 }
             }
         }
+        enableSubmitButton()
     }
     
     @IBAction func doneEditingEmailConfirm(_ sender: UITextField) {
@@ -50,18 +51,21 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
             self.redPulse(sender)
             self.redPulse(self.emailTextField)
         }
+        enableSubmitButton()
     }
     
     @IBAction func doneEditingPhone(_ sender: UITextField) {
-        if self.phoneTextField.text?.characters.count != 7 {
+        if (self.phoneTextField.text?.characters.count)! >= 7 {
             self.redPulse(sender)
         }
+        enableSubmitButton()
     }
     
     @IBAction func doneEditingPassword(_ sender: UITextField) {
         if (self.passwordTextField.text?.characters.count)! < 6 {
             self.redPulse(sender)
         }
+        enableSubmitButton()
     }
     
     
@@ -69,6 +73,7 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
         if self.passwordConfirmTextField.text != self.passwordTextField.text {
             self.redPulse(sender)
         }
+        enableSubmitButton()
     }
     
     func redPulse(_ sender: UITextField) {
@@ -86,6 +91,12 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
                 sender.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
             })
         })
+    }
+    
+    func enableSubmitButton() {
+        if self.passwordConfirmTextField.text! == self.passwordTextField.text! && (self.phoneTextField.text?.characters.count)! >= 7 && emailConfirmationTextField.text! == emailTextField.text! && (self.passwordConfirmTextField.text?.characters.count)! > 1{
+            self.submitButton.isEnabled = true
+        }
     }
     
 }
