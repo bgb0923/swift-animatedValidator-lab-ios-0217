9d0efd74907995e2dac806d8b8b94f2245c251a8
bgb0923 <bgb0923@gmail.com>
2017-02-28 12:45:38 -0500

    Done

--- animated-validator-swift/ValidatorViewController.swift
+++ animated-validator-swift/ValidatorViewController.swift
@@ -7,19 +7,20 @@
 //
 
 import UIKit
+import Darwin
 
 class ValidatorViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var emailConfirmationTextField: UITextField!
     @IBOutlet weak var phoneTextField: UITextField!
     @IBOutlet weak var submitButton: UIButton!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var passwordConfirmTextField: UITextField!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         // Do any additional setup after loading the view, typically from a nib.
         self.submitButton.accessibilityLabel = Constants.SUBMITBUTTON
         self.emailTextField.accessibilityLabel = Constants.EMAILTEXTFIELD
@@ -30,5 +31,61 @@ class ValidatorViewController: UIViewController, UITextFieldDelegate {
         
         self.submitButton.isEnabled = false
     }
-
+    
+    @IBAction func doneEditingEmail(_ sender: UITextField) {
+        if let email = self.emailTextField.text {
+            if !email.contains("@") || !email.contains(".") {
+                self.redPulse(sender)
+            } else if emailConfirmationTextField.text != "" {
+                if email != emailConfirmationTextField.text {
+                    self.redPulse(sender)
+                    self.redPulse(self.emailConfirmationTextField)
+                }
+            }
+        }
+    }
+    
+    @IBAction func doneEditingEmailConfirm(_ sender: UITextField) {
+        if emailConfirmationTextField.text != emailTextField.text && emailTextField.text != "" {
+            self.redPulse(sender)
+            self.redPulse(self.emailTextField)
+        }
+    }
+    
+    @IBAction func doneEditingPhone(_ sender: UITextField) {
+        if self.phoneTextField.text?.characters.count != 7 {
+            self.redPulse(sender)
+        }
+    }
+    
+    @IBAction func doneEditingPassword(_ sender: UITextField) {
+        if (self.passwordTextField.text?.characters.count)! < 6 {
+            self.redPulse(sender)
+        }
+    }
+    
+    
+    @IBAction func doneEditingConfirmPassword(_ sender: UITextField) {
+        if self.passwordConfirmTextField.text != self.passwordTextField.text {
+            self.redPulse(sender)
+        }
+    }
+    
+    func redPulse(_ sender: UITextField) {
+        UIView.animate(withDuration: 0.5, animations: { _ in
+            sender.backgroundColor = UIColor.red
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.5, animations: {_ in
+                sender.backgroundColor = self.view.backgroundColor
+            })
+        })
+        UIView.animate(withDuration: 0.5, animations: { _ in
+            sender.transform = CGAffineTransform(scaleX: 2.0, y: 1.0)
+        }, completion: { _ in
+            UIView.animate(withDuration: 0.5, animations: {_ in
+                sender.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+            })
+        })
+    }
+    
 }
